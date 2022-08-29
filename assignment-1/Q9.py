4b79d03b7472671792fa977050e77f283a4e059c
asteekgoswami <asteekgoswami650@gmail.com>
2022-08-29 17:38:59 +0530

    assignmet-1

---
+++ assignment-1/Q9.py
@@ -0,0 +1,9 @@
+name=input("Enter your name :")
+age=int(input("Enter your age :"))
+qualification=input("Enter your qualification :")
+yop=input("Enter your years of experience :")
+
+print('Name :', name)
+print('Age :',age)
+print('Qualification :',qualification)
+print('Year of experience :',yop)
