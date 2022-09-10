89437ca5965948e9bdc1da922f1cdc8fd86c7994
asteekgoswami <asteekgoswami650@gmail.com>
2022-09-10 23:45:18 +0530

    assignment-2/3

---
+++ assignment-2/A1.py
@@ -0,0 +1,1 @@
+x='hey i am x variable in A1 file'
---
+++ assignment-3/Q1.py
@@ -0,0 +1,5 @@
+num=int(input('enter a number :'))
+print('Type of ',num,' : ',type(num))
+
+ss=str(num)
+print('After conersion from int to str type of ',ss,' :',type(ss))
