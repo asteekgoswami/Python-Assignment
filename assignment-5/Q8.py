c6bcdf316bcad54bc53a4e8c15bbfb9c89b09b21
asteekgoswami <asteekgoswami650@gmail.com>
2022-09-11 22:30:43 +0530

    assignment-5

---
+++ assignment-5/Q8.py
@@ -0,0 +1,4 @@
+#  Write a python script to use IN operator to display the data present in the list
+lis=eval(input("Enter the list :"))
+num=int(input("Enter any number to check that it is present in list or not : "))
+print(num in lis)
