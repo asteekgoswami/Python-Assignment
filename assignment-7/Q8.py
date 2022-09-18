fa1ba0813350a19f6fa92a115b1683bdf7307bcc
asteekgoswami <asteekgoswami650@gmail.com>
2022-09-18 21:57:12 +0530

    assignment-8

--- assignment-7/Q8.py
+++ assignment-7/Q8.py
@@ -1,3 +1,7 @@
+# Write a python script to check whether two given strings are identical, first string
+# comes before the second in dictionary order or first string comes after the second
+# string in dictionary order using match case statement
+
 s1=input("Enter the first string : ")
 s2=input("Enter the second string : ")
 match (s1,s2):
--- assignment-7/Q9.py
+++ assignment-7/Q9.py
@@ -1,3 +1,9 @@
+# Write a python script to check whether a given year is
+# a. Non century leap year
+# b. Century leap year
+# c. Non century non leap year
+# d. Century non leap year
+
 year=int(input("Enter any year : "))
 match year:
     case year if (year%4==0 and year%100!=0):
